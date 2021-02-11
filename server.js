538808a93188eaa8606daad03baaf93e5a106493
designeng <denissavenok@gmail.com>
2021-02-11 15:05:38 +0300

    Remove async wrapper - knex client is sync

--- server.js
+++ server.js
@@ -11,43 +11,41 @@ const knex = require('knex')({
     },
 });
 
-(async function() {
-    router.on('GET', '/', (request, response) => {
-        response.setHeader('Content-Type', 'text/plain');
-        response.end('Main page');
-    })
+router.on('GET', '/', (request, response) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Main page');
+})
 
-    router.on('GET', '/api/v1/users', async (request, response, params) => {
-        const users = await knex(USERS);
-        response.setHeader('Content-Type', 'application/json');
-        response.write(JSON.stringify(users));
-        response.end();
-    })
+router.on('GET', '/api/v1/users', async (request, response, params) => {
+    const users = await knex(USERS);
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(users));
+    response.end();
+})
 
-    router.on('GET', '/api/v1/users/:id', async (request, response, params) => {
-        const { id } = params;
+router.on('GET', '/api/v1/users/:id', async (request, response, params) => {
+    const { id } = params;
 
-        var user = await knex(USERS).where('id', id);
+    var user = await knex(USERS).where('id', id);
 
-        response.setHeader('Content-Type', 'application/json');
-        response.write(JSON.stringify(user));
-        response.end();
-    })
+    response.setHeader('Content-Type', 'application/json');
+    response.write(JSON.stringify(user));
+    response.end();
+})
 
-    router.on('GET', '*', (request, response) => {
-        response.setHeader('Content-Type', 'text/plain');
-        response.end('Not found');
-    })
+router.on('GET', '*', (request, response) => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Not found');
+})
 
-    const server = http.createServer((request, response) => {
-        router.lookup(request, response);
-    })
+const server = http.createServer((request, response) => {
+    router.lookup(request, response);
+})
 
-    server.listen(PORT, err => {
-        if (err) {
-            throw err;
-        } else {
-            console.log(`Server listening on: http://localhost:${PORT}`);
-        }
-    })
-})();
+server.listen(PORT, err => {
+    if (err) {
+        throw err;
+    } else {
+        console.log(`Server listening on: http://localhost:${PORT}`);
+    }
+})
